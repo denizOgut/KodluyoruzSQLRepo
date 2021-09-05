6994cd6ed98716b2b875ce4defb9939fbbff3764
denizOgut <46030809+denizOgut@users.noreply.github.com>
2021-09-05 23:51:36 +0300

    Ödev 2
    Ödev 2 SQL sorguları

---
+++ Ödev 2.sql
@@ -0,0 +1,3 @@
+--SELECT * FROM film WHERE  film.replacement_cost BETWEEN  12.99 AND   16.99-0.1;
+--SELECT first_name , last_name FROM actor WHERE actor.first_name IN ('Penelope','Nick','Ed');
+--SELECT * FROM film WHERE film.rental_rate IN (0.99, 2.99, 4.99) AND film.replacement_cost IN (12.99, 15.99, 28.99);
